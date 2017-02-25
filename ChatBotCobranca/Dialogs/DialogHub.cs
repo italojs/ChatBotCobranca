4f78e42795917223d4e8af43f99e89e076d7a422
italoJs <italo.i@live.com>
2017-02-25 13:27:25 -0300

    add some comments

--- ChatBotCobranca/Dialogs/DialogHub.cs
+++ ChatBotCobranca/Dialogs/DialogHub.cs
@@ -21,21 +21,26 @@ namespace ChatBotCobranca.Dialogs
     {
         
 
-
+        //Luis intent
         [LuisIntent("VerFatura")]
+        //"IDialogContext context" is like some data's activity
+        //"LuisResult result" is your Luis.ai JSON
         public async Task VerFatura(IDialogContext context, LuisResult result)
         {
-            
+            //sending a message
             await context.PostAsync("Só um minuto por favor, estou consultando.");
+            //seting a time only for the bot looks like it is typing
             Thread.Sleep(3000);
             await context.PostAsync($"Sua fatura está em R$ R$ 500,00 senhor.");
             Thread.Sleep(3000);
             await context.PostAsync("Posso ajudar em mais alguma coisa senhor?");
 
+            //waitting a another message
             context.Wait(MessageReceived);
 
         }
 
+        //another intent
         [LuisIntent("EnviaEmail")]
         public async Task EnviaEmail(IDialogContext context, LuisResult result)
         {
